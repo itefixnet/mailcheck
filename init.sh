13ab686b724e1f230cf97f9a207ded239078bf59
Tevfik Karagulle <tk@itefix.net>
2025-12-15 09:27:14 +0100

    chore: remove old unused scripts and docker-compose.yml

--- init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Start shell2http server
-exec shell2http -port 8080 \
-    -export-all-vars \
-    -form \
-    / 'cat /app/frontend/index.html' \
-    /api/check 'bash -c /app/scripts/check.sh'
